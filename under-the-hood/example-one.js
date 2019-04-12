6053bb503a1327cffa1fa4bd630866c5e84975d6
Brodey Newman <brodeynewman@gmail.com>
2019-04-11 20:58:07 -0400

    example one change

--- under-the-hood/example-one.js
+++ under-the-hood/example-one.js
@@ -4,6 +4,10 @@ setTimeout(() => {
  console.log('Invocation: 2');
 }, 0);
 
+setImmediate(() => {
+  console.log('Setting Immediate');
+});
+
 console.log('Invocation: 3');
 
 process.nextTick(() => {
